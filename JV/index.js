ec04038fcd4c17423b60077c37d2e40ad098c942
XcPlease <84813600+XcPlease@users.noreply.github.com>
2022-06-19 04:03:40 +0300

    Add files via upload

---
+++ JV/index.js
@@ -0,0 +1,49 @@
+
+
+cred = {
+    username: "idan",
+    password: "idan",
+}
+
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+  
+
+async function validate() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+
+        if(password == cred.password && username == cred.username) {
+            console.log("Yes")
+            Swal.fire('Signed In successfully!', 'Redirecting..', 'success')    
+            await sleep(700)
+            window.location.href = "/index.html";
+
+
+        } else {
+            Swal.fire(`Couldn't Sign In..`, 'Try Again..', 'error')    
+
+        }
+
+
+    }
+
+
+
+
+async function rev() {
+    const password = document.querySelector("#password");
+    const type = password.getAttribute("type");
+    await sleep(700)
+    password.setAttribute("type", "text");
+
+}
+
+
+
+
+
+
